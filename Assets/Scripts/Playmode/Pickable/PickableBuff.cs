56f026f3ebcd56dea320019257277333b00103e5
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-23 14:09:17 -0400

    Pickable added

---
+++ Assets/Scripts/Playmode/Pickable/PickableBuff.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+    public class PickableBuff : Pickable
+    {
+        enum type
+        {
+            MedKit,
+            Star
+        }
+        protected override void GetPicked(Collision2D other)
+        {
+            //buff the other
+        }
+
+        protected override void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.CompareTag("Enemy"))
+            {
+                GetPicked(other);
+            }
+        }
+    }
---
+++ Assets/Scripts/Playmode/Pickable/PickableWeapon.cs
@@ -0,0 +1,29 @@
+﻿using Playmode.Ennemy.BodyParts;
+using UnityEngine;
+
+public class PickableWeapon : Pickable
+{
+    enum WeaponType
+    {
+        Shotgun,
+        Uzi
+    }
+
+    [SerializeField] private WeaponType weaponType;
+    [SerializeField] private int damage = 1;
+
+    protected override void GetPicked(Collision2D other)
+    {
+        //add to hand
+        other.transform.GetComponent<HandController>().Hold(this.gameObject);
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Enemy"))
+        {
+            GetPicked(other);
+            Destroy(this);
+        }
+    }
+}
