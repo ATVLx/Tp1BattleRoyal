53457a3f524067e330eeb5f92efb93719bf34d56
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-23 14:11:51 -0400

    Pickable added

---
+++ Assets/Scripts/Playmode/Pickable/Pickable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Playmode.Util.Values;
+using UnityEngine;
+
+public abstract class Pickable : MonoBehaviour
+{
+    protected abstract void OnCollisionEnter2D(Collision2D other);
+
+    protected abstract void GetPicked(Collision2D other);
+
+}
