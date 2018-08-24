038f36308b2789b800775ef18380f684733d95e2
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-24 11:55:00 -0400

    bug fix

--- Assets/Scripts/Playmode/Pickable/PickableBuff.cs
+++ Assets/Scripts/Playmode/Pickable/PickableBuff.cs
@@ -20,7 +20,7 @@ public class PickableBuff : Pickable
 
     protected override void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("Ennemy"))
         {
             GetPicked(other);
         }
