9912fc7e4a52df028c8e41431f4c23a1a488601e
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-24 11:51:14 -0400

    Camera edge is the map edge

--- Assets/Scripts/Playmode/Pickable/PickableGenerator.cs
+++ Assets/Scripts/Playmode/Pickable/PickableGenerator.cs
@@ -1,30 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using Playmode.Application;
 using UnityEngine;
 
 public class PickableGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject[] pickablePrefabs;
+    [SerializeField] private int pickableNumber;
 
-	[SerializeField] private GameObject[] pickablePrefabs;
-	[SerializeField] private int pickableNumber;
-	[SerializeField] private int maxSpawnPosY;
-	[SerializeField] private int minSpawnPosX;
-	[SerializeField] private int minSpawnPosY;
-	[SerializeField] private int maxSpawnPosX;
-	private UnityEngine.Random rnd;
-	
-	
-	// Use this for initialization
-	void Start () {
-		rnd=new Random();
-		for (int i = 0; i < pickableNumber; i++)
-		{
-			Instantiate(
-				pickablePrefabs[Random.Range(0,pickablePrefabs.Length)],
-				new Vector3(Random.Range(minSpawnPosX,maxSpawnPosX),Random.Range(minSpawnPosY,maxSpawnPosY),0),
-				Quaternion.identity,
-				transform);
-		}
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        float maxSpawnPosY = Camera.main.GetComponent<CameraEdge>().Height / 2;
+        float minSpawnPosX = -Camera.main.GetComponent<CameraEdge>().Width / 2;
+        float minSpawnPosY = -Camera.main.GetComponent<CameraEdge>().Height / 2;
+        float maxSpawnPosX = Camera.main.GetComponent<CameraEdge>().Width / 2;
+
+        for (int i = 0; i < pickableNumber; i++)
+        {
+            Instantiate(
+                pickablePrefabs[Random.Range(0, pickablePrefabs.Length)],
+                new Vector3(Random.Range(minSpawnPosX, maxSpawnPosX), Random.Range(minSpawnPosY, maxSpawnPosY), 0),
+                Quaternion.identity,
+                transform);
+        }
+    }
 }
