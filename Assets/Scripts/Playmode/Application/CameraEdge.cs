72f30bf0427bcb693c9503a162585e8eb97fcb92
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-24 11:51:37 -0400

    Camera edge is the map edge

---
+++ Assets/Scripts/Playmode/Application/CameraEdge.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Playmode.Application
+{
+    public class CameraEdge : MonoBehaviour
+    {
+        public float Height => 2f * Camera.main.orthographicSize;
+        public float Width =>  Height * Camera.main.aspect;
+    }
+}
