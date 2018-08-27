a24bbf949c6d9d28bedcb859889a48d64e1ac6dd
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-27 09:54:43 -0400

    Camera shrink

--- Assets/Scripts/Playmode/Application/CameraController.cs
+++ Assets/Scripts/Playmode/Application/CameraController.cs
@@ -18,7 +18,7 @@ public class CameraController : MonoBehaviour
 	{
 			
 		if (currentCameraSizeGoal != minimumCameraSize)
-			currentCameraSizeGoal -= (Camera.main.orthographicSize/numberEnnemyRemaining)-minimumCameraSize;
+			currentCameraSizeGoal -= (Camera.main.orthographicSize/numberEnnemyRemaining);
 		if (currentCameraSizeGoal < minimumCameraSize)
 			currentCameraSizeGoal= minimumCameraSize;
 	}
