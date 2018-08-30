ab76996d8ef38f31daa5bc237df772c388ffeda8
Gabriel Bouchard <gaboutch@outlook.com>
2018-08-30 13:40:29 -0400

    death eventCHannel

---
+++ Assets/Scripts/Playmode/Application/EnnemyDeathEventChannel.cs
@@ -0,0 +1,21 @@
+﻿using Playmode.Ennemy;
+using UnityEngine;
+
+namespace Playmode.Application
+{
+    public delegate void EnnemyDeathEventHandler(EnnemyController ennemyController);
+    public class EnnemyDeathEventChannel : MonoBehaviour
+    {
+        public event EnnemyDeathEventHandler OnEnnemyDie;
+
+        public void OnDeath(EnnemyController ennemyController)
+        {
+            NotifyEnnemyDied(ennemyController);
+        }
+
+        private void NotifyEnnemyDied(EnnemyController ennemyController)
+        {
+            if (OnEnnemyDie!= null) OnEnnemyDie(ennemyController);
+        }
+    }
+}
